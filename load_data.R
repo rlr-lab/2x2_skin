456f9103af5de8f2ed08b772056e98e98e34a704
pascoesean <seanpascoe2024@u.northwestern.edu>
2023-11-06 15:48:34 -0600

    initial commit from rstudio

---
+++ load_data.R
@@ -0,0 +1,27 @@
+### LOADING DATA ----
+
+# load packages ----
+library(tidyverse)
+
+# does cell population change between circumsized and not? glans (<-) vs shaft (should be control?) ? 
+# tissue infected in lab -> paired samples
+
+
+# experiments x donor ----
+
+exp_donor <- readxl::read_excel("data/ExperimentsxDonor.xlsx")
+
+
+# R21_path core
+
+r21 <- read_csv("data/R21_PathCore.csv")
+
+
+# Target cells R21
+
+target_dist <- read_csv("data/Target cells R21 DISTANCE.csv")
+target_total <- read_csv("data/Target cells R21 TOTAL COUNT.csv") # counts and some summary stats about area of tissue section for above
+
+# virus count R21 ----
+
+virus <- read_csv("data/Virus count - R21.csv")
